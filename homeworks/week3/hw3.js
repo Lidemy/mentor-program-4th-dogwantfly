920faf3fe6270c5292417ba044d696cfbb247059
hsin-yu <tina2793778@gmail.com>
2020-09-16 22:13:06 +0800

    hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,4 +1,5 @@
 function isPrime(num) {
+  if (num === 1) return false;
   for (let i = 2; i < num; i += 1) {
     if (Number(num) % i === 0) {
       return false;
@@ -8,17 +9,7 @@ function isPrime(num) {
 }
 function solve(lines) {
   for (let i = 1; i < lines.length; i += 1) {
-    if (Number(lines[i]) === 1) {
-      console.log('Composite');
-    } else if (Number(lines[i]) === 2) {
-      console.log('Prime');
-    } else if (Number(lines[i]) > 2) {
-      if (isPrime(lines[i])) {
-        console.log('Prime');
-      } else {
-        console.log('Composite');
-      }
-    }
+    console.log(isPrime(Number(lines[i])) ? 'Prime' : 'Composite');
   }
 }
 solve(['5', '1', '2', '3', '4', '5']);
