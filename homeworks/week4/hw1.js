826c3036be1a09cc33b054c3538c70808f88af87
hsin-yu <tina2793778@gmail.com>
2020-10-11 17:20:11 +0800

    hw1 修改

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -3,13 +3,19 @@ const request = require('request');
 request(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
-    try {
-      const json = JSON.parse(body);
-      for (let i = 0; i < json.length; i += 1) {
-        console.log(`${json[i].id}  ${json[i].name}`);
-      }
-    } catch (e) {
-      console.log(e); // 若回傳不是 JSON 格式字串就印出來
+    if (error) {
+      return console.log('抓取失敗', error);
     }
+    let data;
+    try {
+      data = JSON.parse(body);
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+    for (let i = 0; i < data.length; i += 1) {
+      console.log(`${data[i].id}  ${data[i].name}`);
+    }
+    return body;
   },
 );
