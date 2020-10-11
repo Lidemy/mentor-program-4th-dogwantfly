c20eaa37203a28fbd547766ce954592400075441
hsin-yu <tina2793778@gmail.com>
2020-10-11 18:48:19 +0800

    hw2 修改

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,108 +1,127 @@
 const request = require('request');
 const process = require('process');
 
-if (process.argv[2] === 'list') {
+const action = process.argv[2];
+const params = process.argv[3];
+
+const listBooks = () => {
   request(
     'https://lidemy-book-store.herokuapp.com/books?_limit=20',
     (error, response, body) => {
-      try {
-        const json = JSON.parse(body);
-        for (let i = 0; i < json.length; i += 1) {
-          console.log(`${json[i].id}  ${json[i].name}`);
-        }
-      } catch (e) {
-        console.log(e); // 若回傳不是 JSON 格式字串就印出來
+      if (error) {
+        return console.log('抓取失敗', error);
       }
+      let data;
+      try {
+        data = JSON.parse(body);
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+      for (let i = 0; i < data.length; i += 1) {
+        console.log(`${data[i].id}  ${data[i].name}`);
+      }
+      return data;
     },
   );
-}
-if (process.argv[2] === 'read') {
+};
+const readBook = (id) => {
   request(
-    'https://lidemy-book-store.herokuapp.com/books?_limit=20',
+    `https://lidemy-book-store.herokuapp.com/books/${id}`,
     (error, response, body) => {
-      try {
-        const json = JSON.parse(body);
-        const id = Number(process.argv[3]) - 1;
-        console.log(json[id].name);
-      } catch (e) {
-        console.log(e); // 若回傳不是 JSON 格式字串就印出來
+      if (error) {
+        return console.log('抓取失敗', error);
       }
+      let data;
+      try {
+        data = JSON.parse(body);
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+      console.log(data.name);
+      return data;
     },
   );
-}
-if (process.argv[2] === 'delete') {
+};
+const deleteBook = (id) => {
   request.delete(
-    `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
-    (error, response) => {
-      if (response.statusCode >= 200 && response.statusCode < 300) {
-        console.log('刪除成功');
+    `https://lidemy-book-store.herokuapp.com/books/${id}`,
+    (error, response, body) => {
+      if (error) {
+        return console.log('刪除失敗', error);
       }
+      console.log('刪除成功');
+      return body;
     },
   );
-}
-if (process.argv[2] === 'create') {
+};
+const createBook = (name) => {
   request.post(
     {
       url: 'https://lidemy-book-store.herokuapp.com/books',
       form: {
-        name: process.argv[3],
+        name,
       },
     },
     (error, response, body) => {
-      if (response.statusCode >= 200 && response.statusCode < 300) {
-        console.log('新增成功');
-        try {
-          let json = JSON.parse(body);
-          request(
-            `https://lidemy-book-store.herokuapp.com/books/${json.id}}`,
-            () => {
-              try {
-                json = JSON.parse(body);
-                console.log(`${json.id}  ${json.name}`);
-              } catch (e) {
-                console.log(e); // 若回傳不是 JSON 格式字串就印出來
-              }
-            },
-          );
-        } catch (e) {
-          console.log(e); // 若回傳不是 JSON 格式字串就印出來
-        }
-      } else {
-        console.log('新增失敗');
+      if (error) {
+        return console.log('新增失敗', error);
       }
+      console.log('新增成功');
+      let data;
+      try {
+        data = JSON.parse(body);
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+      console.log(`${data.id} ${data.name}`);
+      return data;
     },
   );
-}
-if (process.argv[2] === 'update') {
+};
+const updateBook = (id, bookName) => {
   request.patch(
     {
       url: `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
       form: {
-        name: process.argv[4],
+        name: bookName,
       },
     },
     (error, response, body) => {
-      if (response.statusCode >= 200 && response.statusCode < 300) {
-        console.log('更改成功');
-        try {
-          let json = JSON.parse(body);
-          request(
-            `https://lidemy-book-store.herokuapp.com/books/${json.id}}`,
-            () => {
-              try {
-                json = JSON.parse(body);
-                console.log(`${json.id}  ${json.name}`);
-              } catch (e) {
-                console.log(e); // 若回傳不是 JSON 格式字串就印出來
-              }
-            },
-          );
-        } catch (e) {
-          console.log(e); // 若回傳不是 JSON 格式字串就印出來
-        }
-      } else {
-        console.log('更改失敗');
+      if (error) {
+        return console.log('更改失敗', error);
       }
+      console.log('更改成功');
+      let data;
+      try {
+        data = JSON.parse(body);
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+      console.log(`${data.id}  ${data.name}`);
+      return data;
     },
   );
+};
+switch (action) {
+  case 'list':
+    listBooks();
+    break;
+  case 'read':
+    readBook(params);
+    break;
+  case 'delete':
+    deleteBook(params);
+    break;
+  case 'create':
+    createBook(params);
+    break;
+  case 'update':
+    updateBook(params, process.argv[4]);
+    break;
+  default:
+    console.log('Available commands: list, read, delete, create and update');
 }
