dde06ac4aba5ea4e493827a571d4813774109f2c
hsin-yu <tina2793778@gmail.com>
2020-10-11 16:00:46 +0800

    hw1 修改

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,9 +1,15 @@
-// const request = require('request');
-import request from 'request';
+const request = require('request');
 
-request('https://lidemy-book-store.herokuapp.com/books?_limit=10', (error, response, body) => {
-  const json = JSON.parse(body);
-  for (let i = 0; i < json.length; i += 1) {
-    console.log(`${json[i].id}  ${json[i].name}`);
-  }
-});
+request(
+  'https://lidemy-book-store.herokuapp.com/books?_limit=10',
+  (error, response, body) => {
+    try {
+      const json = JSON.parse(body);
+      for (let i = 0; i < json.length; i += 1) {
+        console.log(`${json[i].id}  ${json[i].name}`);
+      }
+    } catch (e) {
+      console.log(e); // 若回傳不是 JSON 格式字串就印出來
+    }
+  },
+);
